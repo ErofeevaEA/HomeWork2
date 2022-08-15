339b41260bd9b4ccda7e9edc1f293382a2c86611
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-15 19:33:03 +0300

    Exercise 1

---
+++ EX_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Введите трехзначное число = ");
+        string a = Console.ReadLine();
+        Console.WriteLine("Второе число: " + a[1]);
+        Console.ReadKey();
+    }
+}
+
