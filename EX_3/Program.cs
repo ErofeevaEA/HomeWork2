9ff3571f631e7a005dda6c511524749dd591922e
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-15 19:37:47 +0300

    Exercise 3

---
+++ EX_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Введите цифру обозначающую день недели: ");
+        Console.WriteLine("1 - Пн, 2 - Вт, 3 - Ср, 4 - Чт,  5 - Пт, 6 - Сб, 7 - Вс ");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Выходной? ");
+        string[] day = { "нет", "нет", "нет", "нет", "нет", "да", "да" };
+        Console.WriteLine(day[number - 1]);
+
+    }
+
+}
