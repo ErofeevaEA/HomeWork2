3b88328e4a7afb976db6acee8db9e7ebbbc84d14
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-15 19:35:49 +0300

    Exercise 2

---
+++ EX_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Введите число = ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n > -100 && n < 100)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+
+        else
+        {
+            if (n < 0)
+            {
+                n = -1 * n;
+            }
+            string a = Convert.ToString(n);
+            Console.WriteLine("Третье число число: " + a[2]);
+        }
+
+    }
+
+}
